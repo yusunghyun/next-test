ead882b57943df364bd6cfa8741527f28f98913e
yusunghyun <ysh1513@naver.com>
2022-05-21 16:44:45 +0900

    🔧 add first eslint init
    - 기본적인 것만 시작함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const Home: NextPage = (): JSX.Element => {
   return (
     <div className={styles.container}>
       <Header />
