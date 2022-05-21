3a76e5b39585931bf459efd302213251bbf8cdd3
yusunghyun <ysh1513@naver.com>
2022-05-21 16:58:13 +0900

    🔧 add first prettier init

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     // "plugin:react/recommended",
     // "airbnb",
     "airbnb-typescript",
-    // "plugin:prettier/recommended",
+    "plugin:prettier/recommended",
     // "plugin:react/jsx-runtime",
   ],
   parserOptions: {
